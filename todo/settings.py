c339f5e8729a1e4417ac47e9c50b014fc54a3ae3
rishibisht1234 <rishibishtlinkdn@gmail.com>
2025-07-18 12:43:05 +0530

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 import os
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG=True
+DEBUG=False
 
 ALLOWED_HOSTS = ['todoapi-3kjr.onrender.com']
 
